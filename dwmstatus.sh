5cf987aaadc8259eff3ca3209201afeb695ed648
Svyatoslav I. Maslennikov <me@smaslennikov.com>
2019-03-17 00:46:34 -0700

    Clean up status script

--- dwmstatus.sh
+++ dwmstatus.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 while true; do
-	DWM_RENEW_INT=3
+    int=3
 
-	# Volume Level
-	DWM_VOL=$( amixer -c0 sget Master | awk -vORS=' ' '/Mono:/ {print($6$4)}' );
+    vol=$( amixer -c0 sget Master | awk -vORS=' ' '/Mono:/ {print($6$4)}' );
+    clock=$( date '+%e %b %Y %a | %k:%M' );
+    uptime=$( uptime -p );
+    kernel=$( uname -r );
+    cputemp=$(sensors coretemp-isa-0000 | grep Package | awk '{print $4;}')
+    buttery=$(acpi | cut -d',' -f2,3)
 
-	# Date and Time
-	DWM_CLOCK=$( date '+%e %b %Y %a | %k:%M' );
-
-	uptime=$( uptime -p );
-	kernel=$( uname -r );
-	cpuusage=$( grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}' );
-
-        cputemp=$(sensors coretemp-isa-0000 | grep Package | awk '{print $4;}')
+    if [ $(which nvidia-smi) ]; then
         nvidiatemp=$(nvidia-smi -q -d temperature | grep "GPU Current" | cut -d':' -f2)
+        status="$cputemp | gpu$nvidiatemp | $uptime | $vol | buttery$buttery | $clock";
+    else
+        status="$cputemp | $uptime | $vol| buttery$buttery | $clock";
+    fi
 
-        buttery=$(acpi | cut -d',' -f2,3)
-
-	# Overall output command
-	DWM_STATUS="cpu $cputemp | gpu$nvidiatemp | $cpuusage | $uptime | Vol: $DWM_VOL | buttery$buttery | $DWM_CLOCK";
-	xsetroot -name "$DWM_STATUS";
-	sleep $DWM_RENEW_INT;
-done &
+    xsetroot -name "$status";
+    sleep $int;
+done
