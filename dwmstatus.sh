dff096674c87006abd4f9a80a5c2f53b4eb40bce
Svyatoslav I. Maslennikov <me@smaslennikov.com>
2021-02-02 09:41:58 -0800

    Update status script

--- dwmstatus.sh
+++ dwmstatus.sh
@@ -2,19 +2,16 @@
 while true; do
     int=3
 
-    vol=$( amixer -c0 sget Master | awk -vORS=' ' '/Mono:/ {print($6$4)}' );
-    clock=$( date '+%e %b %Y %a | %k:%M' );
-    uptime=$( uptime -p );
-    kernel=$( uname -r );
+    volume_out=$(amixer -c0 sget Master | awk -vORS=' ' '/Mono:/ {print($6$4)}');
+    audio_in=$(amixer -c1 sget Mic | grep 'Mono: Capture' | awk '{print $6;}');
+    clock=$(date '+%e %b %Y %a | %k:%M');
+    uptime=$(uptime -p );
+    kernel=$(uname -r );
     cputemp=$(sensors coretemp-isa-0000 | grep Package | awk '{print $4;}')
-    buttery=$(acpi | cut -d',' -f2,3)
+    buttery=$(upsc ups | grep battery.charge: | sed -e 's/battery.charge: //')
+    nvidiatemp=$(nvidia-smi -q -d temperature | grep "GPU Current" | cut -d':' -f2)
 
-    if [ $(which nvidia-smi) ]; then
-        nvidiatemp=$(nvidia-smi -q -d temperature | grep "GPU Current" | cut -d':' -f2)
-        status="$cputemp | gpu$nvidiatemp | $uptime | $vol | buttery$buttery | $clock";
-    else
-        status="$cputemp | $uptime | $vol| buttery$buttery | $clock";
-    fi
+    status="$cputemp | gpu$nvidiatemp | $uptime | $volume_out| $buttery% | $clock";
 
     xsetroot -name "$status";
     sleep $int;
